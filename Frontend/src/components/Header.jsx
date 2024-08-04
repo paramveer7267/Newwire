f748299312e399cd762f0a3c1d2bbf3536da8cb8
Paramveer Singh <114360831+paramveer7267@users.noreply.github.com>
2024-08-04 06:00:14 +0530

    Update Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -38,6 +38,7 @@ const Header = () => {
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src="https://raw.githubusercontent.com/paramveer7267/logos/main/newwire header.png" width={120} height={30} alt="Newwire" />
+          <span>Newwire</span>
         </a>
 
         <nav
